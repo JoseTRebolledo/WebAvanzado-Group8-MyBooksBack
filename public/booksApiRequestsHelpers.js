7f6babe89644543ca542211d616cd8a4ad694ff6
JoseTRebolledo <jos.rebolledo@uc.cl>
2024-05-30 19:41:30 -0400

    search by title and key of books

---
+++ public/booksApiRequestsHelpers.js
@@ -0,0 +1,56 @@
+const requireFields = ["title", "author_name", "cover_i", "first_publish_year", "key", "first_sentence"];
+const searchJson = "search.json?"
+const booksApiPath = "https://openlibrary.org/"
+const works = "works";
+const json = ".json";
+const standardLimit = 20;
+const standardSortBy = "rating";
+const standardPage = 1;
+
+function replaceSpacesWithPlus(str) {
+    return str.replace(/\s+/g, '+');
+  }
+
+function getTitle(title){
+    return "title=" + replaceSpacesWithPlus(title);
+}
+
+function getFields(){
+    return `&fields=${requireFields.join(',')}`;
+}
+
+function getLimit(limit){
+    return `&limit=${limit}`;
+}
+
+function getSort(sort){
+    return `&sort=${sort}`;
+}
+
+function getPage(page){
+    return `&page=${page}`;
+}
+
+function getSearchByTitleQuery(title, limit=standardLimit, sortedBy=standardSortBy, page=standardPage){
+    const request = `${booksApiPath}${searchJson}${
+        getTitle(title)}${getFields()}${getLimit(limit)}${getSort(sortedBy)}${getPage(page)}`
+    return request;
+}
+
+function calculateMaxPages(items, limit=standardLimit){
+    return Math.ceil(items / standardLimit)
+}
+
+function getSearchByKeyQuery(key){
+    return `${booksApiPath}${key}${json}`;
+}
+
+function getSearchAuthorQuery(key){
+    return `${booksApiPath}${key}${json}`;
+}
+
+module.exports = {
+    getSearchByTitleQuery,
+    calculateMaxPages,
+    getSearchByKeyQuery
+  };
---
+++ routes/books.js
@@ -0,0 +1,68 @@
+const { getSearchByTitleQuery, calculateMaxPages, getSearchByKeyQuery, 
+  getSearchAuthorQuery
+} = require('../public/booksApiRequestsHelpers');
+
+var express = require('express');
+var router = express.Router();
+
+const axios = require('axios');
+
+const standardLimit = 20;
+const standardSortBy = "rating";
+const standardPage = 1;
+
+
+
+router.get('/searchByTitle', async function(req, res) {
+
+  const { title, limit, sortedBy, page} = req.body;
+  const query = getSearchByTitleQuery(title, limit, sortedBy, page);
+
+    try {
+        const apiResponse = await axios.get(query);
+        const maxPages = calculateMaxPages(apiResponse.data.numFound, limit);
+        const response = {
+          books: apiResponse.data.docs,
+          maxPages: maxPages
+        }
+        res.json(response);
+        res.status(200);
+      } catch (error) {
+        console.error('Error al realizar la solicitud a la API externa:', error);
+        res.status(500).json({ error: 'Error al obtener datos de la API externa' });
+      }
+});
+
+router.get('/searchByKey', async function(req, res) {
+
+    const key = req.body.key;
+    const booksQuery = getSearchByKeyQuery(key);
+    try {
+        const apiBookResponse = await axios.get(booksQuery);
+        const authors = await findBookAuthors(apiBookResponse.data.authors);
+        const response = {
+          book: apiBookResponse.data,
+          authors: authors
+        }
+        res.json(response);
+        res.status(200);
+      } catch (error) {
+        console.error('Error al realizar la solicitud a la API externa:', error);
+        res.status(500).json({ error: 'Error al obtener datos de la API externa' });
+      }
+});
+
+async function findBookAuthors(authors){
+  const authorPromises = authors.map(async (authorObj) => {
+    const authorKey = authorObj.author.key;
+    const authorQuery = getSearchByKeyQuery(authorKey);
+    const response = await axios.get(authorQuery);
+    return response.data.name;
+  });
+
+  const authorNames = await Promise.all(authorPromises);
+
+  return authorNames;
+}
+
+module.exports = router;
