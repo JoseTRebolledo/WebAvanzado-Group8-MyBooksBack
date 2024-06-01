4e4d3a650cd8be53f4bd4b4bc12a4cceeb9a1294
Vicente Cruz <vcrb@uc.cl>
2024-06-01 19:46:42 -0400

    fix - fixed GET books to parameters and added cors

--- app/app.js
+++ app/app.js
@@ -1,49 +1,50 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-
-
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+let indexRouter = require("./routes/index");
+let usersRouter = require("./routes/users");
 let booksRouter = require("./routes/books");
 let recomendationRouter = require("./routes/recomendation");
 let listElementRouter = require("./routes/listElement");
 
 var app = express();
 
-const orm = require('./models');
+app.use(cors());
+
+const orm = require("./models");
 
 app.locals.orm = orm;
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 app.use("/books", booksRouter);
 app.use("/recomendations", recomendationRouter);
 app.use("/listElements", listElementRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- app/routes/books.js
+++ app/routes/books.js
@@ -1,53 +1,52 @@
-const { getSearchByTitleQuery, calculateMaxPages, getSearchByKeyQuery, 
-  getSearchAuthorQuery
-} = require('../public/booksApiRequestsHelpers');
+const {
+  getSearchByTitleQuery,
+  calculateMaxPages,
+  getSearchByKeyQuery,
+  getSearchAuthorQuery,
+} = require("../public/booksApiRequestsHelpers");
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const axios = require('axios');
+const axios = require("axios");
 
-
-
-
-router.get('/searchByTitle', async function(req, res) {
-    try {
-        const { title, limit, sortedBy, page} = req.body;
-        const query = getSearchByTitleQuery(title, limit, sortedBy, page);
-        const apiResponse = await axios.get(query);
-        const maxPages = calculateMaxPages(apiResponse.data.numFound, limit);
-        const response = {
-          books: apiResponse.data.docs,
-          maxPages: maxPages
-        }
-        res.json(response);
-        res.status(200);
-      } catch (error) {
-        console.error('Error al realizar la solicitud a la API externa:', error);
-        res.status(500).json({ error: 'Error al obtener datos de la API externa' });
-      }
+router.get("/searchByTitle", async function (req, res) {
+  try {
+    const { title, limit, sortedBy, page } = req.query;
+    const query = getSearchByTitleQuery(title, limit, sortedBy, page);
+    const apiResponse = await axios.get(query);
+    const maxPages = calculateMaxPages(apiResponse.data.numFound, limit);
+    const response = {
+      books: apiResponse.data.docs,
+      maxPages: maxPages,
+    };
+    res.json(response);
+    res.status(200);
+  } catch (error) {
+    console.error("Error al realizar la solicitud a la API externa:", error);
+    res.status(500).json({ error: "Error al obtener datos de la API externa" });
+  }
 });
 
-router.get('/searchByKey', async function(req, res) {
-
-    try {
-        const key = req.body.key;
-        const booksQuery = getSearchByKeyQuery(key);
-        const apiBookResponse = await axios.get(booksQuery);
-        const authors = await findBookAuthors(apiBookResponse.data.authors);
-        const response = {
-          book: apiBookResponse.data,
-          authors: authors
-        }
-        res.json(response);
-        res.status(200);
-      } catch (error) {
-        console.error('Error al realizar la solicitud a la API externa:', error);
-        res.status(500).json({ error: 'Error al obtener datos de la API externa' });
-      }
+router.get("/searchByKey", async function (req, res) {
+  try {
+    const key = req.body.key;
+    const booksQuery = getSearchByKeyQuery(key);
+    const apiBookResponse = await axios.get(booksQuery);
+    const authors = await findBookAuthors(apiBookResponse.data.authors);
+    const response = {
+      book: apiBookResponse.data,
+      authors: authors,
+    };
+    res.json(response);
+    res.status(200);
+  } catch (error) {
+    console.error("Error al realizar la solicitud a la API externa:", error);
+    res.status(500).json({ error: "Error al obtener datos de la API externa" });
+  }
 });
 
-async function findBookAuthors(authors){
+async function findBookAuthors(authors) {
   const authorPromises = authors.map(async (authorObj) => {
     const authorKey = authorObj.author.key;
     const authorQuery = getSearchByKeyQuery(authorKey);
